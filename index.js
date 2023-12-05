c983ff7947ff7570b0f63f1c48fe8f9fd4811301
samirammar <samirammar.officially@gmail.com>
2023-12-06 00:11:17 +0400

    CREATE -> index file

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+import productsRoutes from './routes/products.js';
+import authRoutes from './routes/auth.js';
+import usersRoutes from './routes/user.js';
+
+
+
+configDotenv()
+const app = express();
+
+app.use(express.json({limit: "10mb"}));
+app.use(express.urlencoded({limit: "10mb", extended: true}))
+
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("DB Connected!");
+}).catch((error)=>{
+    console.error(error);
+})
+
+// Routes
+app.use('/api/products', productsRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+
+
+
+// Global handler error
+app.use((error, req, res, next)=>{
+    res.status(500).json({
+        status: error.status || 500,
+        message: error._message || error.message  
+    });
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, ()=>{
+    console.log("Server Running On Port: ", port);
+})
