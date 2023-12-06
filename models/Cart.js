7d4e7c2fe3dffc684d981ba801e4d24311bea372
samirammar <samirammar.officially@gmail.com>
2023-12-06 23:10:42 +0400

    EDIT -> add products field

--- models/Cart.js
+++ models/Cart.js
@@ -2,7 +2,15 @@ import mongoose from 'mongoose';
 
 const CartSchema = new mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId,  ref: 'User'},
-    quantity: {type: Number, default: 1},
+    products: [
+        {
+            cartItem: {
+                type: mongoose.Types.ObjectId,
+                ref: "Product"
+            },
+            quantity: {type: Number, default: 1},
+        }
+    ]
 },{timestamps: true});
 
 export default mongoose.model("Cart", CartSchema);
